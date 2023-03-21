3e6d626ca12c495cda213cb0987468a8a70b41e6
smottu <106876134+smottu@users.noreply.github.com>
2023-03-21 06:42:42 +0000

    FizzBuzz

--- Java.java
+++ Java.java
@@ -1,26 +1,23 @@
 public class FizzBuzz {
-  
-  public static void main(String[] args) {
-    int i =0;
-    
-  int loopCount = 100;
-  int numFizz = 3;
-  int numBuzz =5;
-  String strFizz = "Fizz";
-  String strBuzz = "Buzz";
-  
-  
-〇 ( i = 1; i <= loopCoount; i++) {
-  〇( i % (numFizz * numBuzz ) == 0 ){
-    System.out.println(strFizz +strBuzz);
-  }〇　( i % numFizz == 0) {
-    System.out.println(strFizz);
-    } else if (〇) {
-      System.out.println(strBuzz);
-    } else {
-      System.out.println(i);
-    }
-  }
-}
-  
+	public static void main(String[] args) {
+		int i = 0;
+		int loopCount = 100;
+		int numFizz = 3;
+		int numBuzz = 5;
+		String strFizz = "Fizz";
+		String strBuzz = "Buzz";
+
+
+		for ( i = 1; i <= loopCount; i++) {
+			if ( i % (numFizz * numBuzz) == 0) {
+				System.out.println(strFizz + strBuzz);
+			} else if ( i % numFizz == 0 ) {
+				System.out.println(strFizz);
+			} else if (i % numBuzz == 0) {
+				System.out.println(strBuzz);
+			} else {
+				System.out.println(i);
+			}
+		}
+	}
 }
