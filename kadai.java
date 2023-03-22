48ce7305e3d8c6035d127280e7392bc003072824
smottu <106876134+smottu@users.noreply.github.com>
2023-03-22 14:42:30 +0000

    1-100

---
+++ kadai.java
@@ -0,0 +1,15 @@
+
+public class kadai12 {
+
+	public static void main(String[] args) {
+
+		int x = 0;
+		for(int i = 2; i <= 100; i++) {
+			if( i % 2 == 0) {
+				x += i;
+			}
+		}
+		System.out.println(x);
+	}
+
+}
