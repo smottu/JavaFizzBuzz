7e85e7eac604b0a8be25ace33d30085efac490fa
smottu <106876134+smottu@users.noreply.github.com>
2023-03-23 14:10:29 +0000

    1.2

---
+++ 1-2.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main (String[] args) {　　//クラスブロック
+    System.out.prinln("RPG:スッキリ魔王征伐");  //メソッドブロック
+    System.out.prinln("Ver.0.1 by 湊");
+    System.out.println("＜ただいま鋭意学習・製作中＞");
+    System.out.println("プログラムを終了します");
+  }
+}
