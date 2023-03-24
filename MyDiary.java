7948fce2aeb075b481dacd7728fae2bb5c9d9f84
smottu <106876134+smottu@users.noreply.github.com>
2023-03-24 14:29:32 +0000

    1.2.4

---
+++ MyDiary.java
@@ -0,0 +1,9 @@
+//サンプルプログラム　Main
+//開発者：菅原　作成日：2020年4月
+public class MyDiary{
+  //ここからmainメソッド
+  public static void main (String[] args) {
+    System.out.println("日記ソフト");
+    System.out.println("終了します");
+  })
+}
