ae3bbbb3cd064a41508db6ada9c0e3a86e1c90c3
smottu <106876134+smottu@users.noreply.github.com>
2023-03-27 14:22:10 +0000

    rensyu

---
+++ 1.5.java
@@ -0,0 +1,26 @@
+//1-2
+public class java1_5 {
+
+	public static void main(String[] args) {
+		int a = 3;
+		int b = 5;
+		int c = a*b;
+			System.out.println(c);
+	}
+
+}
+
+//1-3
+
+public class java1_5_3 {
+
+	public static void main(String[] args) {
+		boolean a = true;
+		char b = "駆";
+		double c = 3.14;
+		long d = 314159265853976L
+		String e = "ミナトの攻撃！敵に15ポイントのダメージを与えた";
+		System.out.println(a);
+	}
+
+}
