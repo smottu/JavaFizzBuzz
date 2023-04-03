5063ed22b3647bb16d945f88dcd48e10b65f1236
smottu <106876134+smottu@users.noreply.github.com>
2023-04-03 14:22:24 +0000

    2.5.2

---
+++ 2.5.2.java
@@ -0,0 +1,16 @@
+public class Main {
+  public static void main (String[] args){
+    float f = 3;
+    double d = f;
+    System.out.println(f);
+    System.out.println(d);
+  }
+}
+
+
+//2.6
+public class Main {
+  public static void main (String[] args) {
+    int i = 3.2;
+  }
+}
